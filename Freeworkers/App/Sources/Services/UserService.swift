12ef0a7dbd981d99acc187cac62d1d99d3e241e0
hankyeol-dev <ddovlek@kakao.com>
2024-11-12 10:49:32 +0900

    [ProfileView] 유저 정보 업데이트 서비스 로직 반영

--- Freeworkers/App/Sources/Services/UserService.swift
+++ Freeworkers/App/Sources/Services/UserService.swift
@@ -5,6 +5,9 @@ import Combine
 
 protocol UserServiceType {
    func getMe() async -> AnyPublisher<MeViewItem, ServiceErrors>
+   
+   func putNickname(nickname : String) async -> Result<Bool, ServiceErrors>
+   func putPhone(phone : String) async -> Result<Bool, ServiceErrors>
 }
 
 struct UserService : UserServiceType {
@@ -13,7 +16,9 @@ struct UserService : UserServiceType {
    init(userRepository: UserRepositoryType) {
       self.userRepository = userRepository
    }
-   
+}
+
+extension UserService { 
    func getMe() async -> AnyPublisher<MeViewItem, ServiceErrors> {
       let output = await userRepository.getMe()
       
@@ -25,5 +30,27 @@ struct UserService : UserServiceType {
             promise(.failure(.error(message: errors.errorMessage)))
          }
       }.eraseToAnyPublisher()
+   }
+}
+
+extension UserService {
+   func putNickname(nickname: String) async -> Result<Bool, ServiceErrors> {
+      let output = await userRepository.putNickname(nickname: nickname)
+      switch output {
+      case .success:
+         return .success(true)
+      case let .failure(errors):
+         return .failure(.error(message: errors.errorMessage))
+      }
+   }
+   
+   func putPhone(phone: String) async -> Result<Bool, ServiceErrors> {
+      let output = await userRepository.putPhone(phone: phone)
+      switch output {
+      case .success:
+         return .success(true)
+      case let .failure(errors):
+         return .failure(.error(message: errors.errorMessage))
+      }
    }
 }
