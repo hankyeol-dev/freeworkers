e05a5a963edcf9de352c49dd2740ab952b021ada
hankyeol-dev <ddovlek@kakao.com>
2024-11-15 15:33:02 +0900

    [+Date] ISO 8601 형태로 포맷팅하던 날짜 문자열 값을 -시간전, -분전 형태로 더 세분해서 포맷

--- Freeworkers/App/Sources/Views/Utils/Extensions/+Date&String.swift
+++ Freeworkers/App/Sources/Views/Utils/Extensions/+Date&String.swift
@@ -4,17 +4,41 @@ import Foundation
 
 extension String {
    func toISO860() -> Date {
+      let target : Self = String(self.split(separator: ".")[0]) + "Z"
       let formatter = ISO8601DateFormatter()
-      formatter.timeZone = .autoupdatingCurrent
-      return formatter.date(from: self) ?? Date()
+      formatter.timeZone = TimeZone(abbreviation: "KST")
+      formatter.formatOptions = [.withInternetDateTime]
+      return formatter.date(from: target) ?? Date()
    }
 }
 
 extension Date {
    func toChatDate() -> String {
       let formatter = DateFormatter()
-      formatter.locale = .autoupdatingCurrent
-      formatter.dateFormat = "MM월 dd일"
+      let calendar = Calendar.current
+      formatter.locale = Locale(identifier: "ko_KR")
+      formatter.timeZone = TimeZone(abbreviation: "KST")
+     
+      if calendar.isDateInToday(self) {
+         let component = calendar.dateComponents([.hour, .minute], from: self, to: .now)
+         if let hour = component.hour, let minute = component.minute {
+            if hour == 0 && minute == 0 { return "지금" }
+            if hour == 0 && minute != 0 {
+               return "\(minute)분 전"
+            }
+            if hour != 0 && minute == 0 {
+               formatter.dateFormat = "hh시간"
+               return "\(hour)시간 전"
+            }
+            if hour != 0 && minute != 0 {
+               formatter.dateFormat = "hh시간 mm분"
+               return formatter.string(from: self) + " 전"
+            }
+         }
+      } else {
+         formatter.dateFormat = "MM월 dd일"
+      }
+            
       return formatter.string(from: self)
    }
 }
